7c1ddb3974ab7c6a4e499f6848cc5ec114a3fd92
lmotp <lmotp@daum.net>
2022-12-23 00:40:49 +0900

    GroupPurchaseCard,PopularList style modify

--- src/components/PhotoCard/GroupPurchaseCard.tsx
+++ src/components/PhotoCard/GroupPurchaseCard.tsx
@@ -1,15 +1,37 @@
 import styled from "styled-components";
+import Avatar, { genConfig } from "react-nice-avatar";
 
 function GroupPurchaseCard() {
-  const img = "https://i.pinimg.com/550x/15/be/76/15be765d8b4402921d45fe1536115e28.jpg";
+  const config = genConfig();
+  const img = "https://source.unsplash.com/random/300×300";
+  const test = new Array(4).fill("");
+
   return (
     <Card beforesrc={img}>
       <CardInfo>
-        <InfoTitle>2022 마마무 싱글 분철 구함</InfoTitle>
-        <InfoExplanation>적당히 신청 주세요~~ 경험 많습니다.ABCDEFGHIJKLMNOPQRSTUVWXYZ</InfoExplanation>
+        <InfoThumbnail src={img} alt="random" />
+        <InfoWrap>
+          <InfoTop>
+            <InfoCount>
+              <span>01/04</span>
+            </InfoCount>
+            <InfoTitle>2022 마마무 싱글 분철 구함</InfoTitle>
+          </InfoTop>
+          <InfoExplanation>적당히 신청 주세요~~ 경험 많습니다.ABCDEFGHIJKLMNOPQRSTUVWXYZ</InfoExplanation>
+        </InfoWrap>
       </CardInfo>
       <CardMemberWrap>
-        <CardMember></CardMember>
+        {test.map((item, index) => {
+          return (
+            <CardMember key={index}>
+              <UserProfileImg {...config} />
+              <UserInfo>
+                <UserNickName>안녕하세요를레히호</UserNickName>
+                <UserExplanation>보여드리겠습니다안녕하세요 한줄요약해서 보여드리겠습니다? 아닌가요?</UserExplanation>
+              </UserInfo>
+            </CardMember>
+          );
+        })}
       </CardMemberWrap>
     </Card>
   );
@@ -20,8 +42,6 @@ const Card = styled.li<{ beforesrc: string }>`
   min-width: 200px;
   max-width: 296px;
   width: 100%;
-  height: 422px;
-  overflow: hidden;
   border-radius: 12px;
   border: 1px solid rgba(176, 179, 188, 0.1);
 
@@ -36,40 +56,55 @@ const Card = styled.li<{ beforesrc: string }>`
     background-position: 50% 50%;
     background-size: 100%;
     filter: blur(6px);
-    opacity: 0.25;
-    width: 110%;
-    height: 110%;
+    opacity: 0.2;
+    width: 100%;
+    height: 100%;
     transform: translate(-50%, -50%);
   }
 `;
 
 const CardInfo = styled.div`
   position: relative;
+  display: flex;
+  gap: 10px;
   padding: 16px;
-  background: hsla(0, 0%, 100%, 0.5);
-
-  &::before {
-    content: "▶";
-    position: absolute;
-    font-size: 16px;
-    right: 16px;
-    top: 50%;
-    transform: translateY(-50%);
-    color: #6f6f6f;
-  }
+  background: #ffffff80;
 `;
-
+const InfoThumbnail = styled.img`
+  flex-shrink: 0;
+  width: 60px;
+  height: 76px;
+  border-radius: 4px;
+`;
+const InfoWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: calc(100% - 60px);
+  padding-right: 10px;
+`;
+const InfoTop = styled.div`
+  padding-top: 2px;
+`;
+const InfoCount = styled.div`
+  font-size: 12px;
+  margin-bottom: 4px;
+`;
 const InfoTitle = styled.strong`
+  display: inline-block;
+  width: 100%;
   font-weight: 700;
-  font-size: 16px;
-  line-height: 22px;
+  font-size: 14px;
+  line-height: 16px;
   color: #242424;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 `;
 const InfoExplanation = styled.p`
-  margin-top: 4px;
   font-weight: 400;
-  font-size: 14px;
-  line-height: 20px;
+  font-size: 12px;
+  line-height: 1.5;
   color: #6f6f6f;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -77,11 +112,48 @@ const InfoExplanation = styled.p`
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   word-wrap: break-word;
-  max-height: 36px;
-  padding-right: 36px;
+  max-height: 34px;
 `;
 
-const CardMemberWrap = styled.ul``;
-const CardMember = styled.li``;
+const CardMemberWrap = styled.ul`
+  padding: 0 16px;
+  max-height: 280px;
+  overflow-y: auto;
+  overscroll-behavior: none;
+`;
+const CardMember = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 16px 0;
+  border-bottom: 1px solid #eeeeee;
+`;
+const UserProfileImg = styled(Avatar)`
+  width: 44px;
+  height: 44px;
+  flex-shrink: 0;
+`;
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const UserNickName = styled.em`
+  margin-bottom: 4px;
+  font-size: 12px;
+  font-weight: 700;
+`;
+const UserExplanation = styled.p`
+  font-weight: 400;
+  font-size: 11px;
+  line-height: 1.5;
+  color: #6f6f6f;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+  max-height: 32px;
+`;
 
 export default GroupPurchaseCard;
--- src/components/common/Popluar/PopularList.tsx
+++ src/components/common/Popluar/PopularList.tsx
@@ -7,6 +7,10 @@ function PopularList() {
       <ListImgWrap to="/" beforesrc={"https://pbs.twimg.com/media/EWNj92iU4AAdPEb.jpg"}>
         <ListImg src={"https://pbs.twimg.com/media/EWNj92iU4AAdPEb.jpg"} />
       </ListImgWrap>
+      <ListInfo>
+        <InfoTitle>안녕하세요로를레히오</InfoTitle>
+        <InfoExplanation>적당히해주세요오오오오오오오오오옹가리 파이야 파이야파파이파이파이리</InfoExplanation>
+      </ListInfo>
     </List>
   );
 }
@@ -20,11 +24,13 @@ const List = styled.li`
 const ListImgWrap = styled(NavLink)<{ beforesrc: string }>`
   position: relative;
   display: block;
-  height: 242px;
+  height: 180px;
   overflow: hidden;
-  border-radius: 12px;
+  border-radius: 12px 12px 0 0;
   background: rgba(176, 179, 188, 0.1);
-  border: 1px solid rgba(176, 179, 188, 0.1);
+  border: 1px solid rgba(176, 179, 188, 0.4);
+  border-bottom: none;
+  box-shadow: 0 2px 3px rgba(176, 179, 188, 0.5);
 
   &::before {
     content: "";
@@ -47,10 +53,42 @@ const ListImgWrap = styled(NavLink)<{ beforesrc: string }>`
 const ListImg = styled.img`
   height: inherit;
   position: absolute;
-  top: 26px;
+  top: 20px;
   left: 50%;
-  transform: translateX(-50%);
+  transform: translateX(-50%) scale(0.82);
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+`;
+
+const ListInfo = styled.div`
+  padding: 16px 12px;
+  border-radius: 0 0 12px 12px;
+  border: 1px solid rgba(176, 179, 188, 0.4);
+  border-top: none;
+`;
+
+const InfoTitle = styled.strong`
+  display: inline-block;
+  width: 100%;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  color: #242424;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+const InfoExplanation = styled.p`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.5;
+  color: #6f6f6f;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+  max-height: 34px;
 `;
 
 export default PopularList;
